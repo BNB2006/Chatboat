7a17bd21c854ffa84b74dfefad71181db492413e
Balaji Bandgar <balajibandgar26@gmail.com>
2025-09-06 10:14:05 +0530

    Initial (project setup)

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function App(){
+
+return(
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-3 bg-blue-200">
+       <p className="text-5xl">Installing Dependencies</p>
+        <span className="text-xl text-gray-500">(project setup...)</span>
+</div>
+)
+}
