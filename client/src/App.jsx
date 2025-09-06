6a1eac896dad4e6cc39d370e260c18a6062efcec
Balaji Bandgar <balajibandgar26@gmail.com>
2025-09-06 11:13:06 +0530

    chatbot layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react"
-export default function App(){
+import Chat from "./components/chat";
 
+export default function App(){
 return(
-    <div className="h-screen w-full flex flex-col items-center justify-center gap-3 bg-blue-200">
-       <p className="text-5xl">Installing Dependencies</p>
-        <span className="text-xl text-gray-500">(project setup...)</span>
-</div>
+    <div className="flex flex-col h-screen">
+        <header className="p-4 bg-gray-900 text-white text-xl font-bold">
+            Siege
+        </header>
+
+        <main className="flex-1 flex-col bg-blue-100">
+            <Chat/>
+        </main>
+    </div>
 )
 }
---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,17 @@
+export default function Chat(){
+return(
+    <div className="flex flex-col flex-1 overflow-hidden">
+
+        <div className="flex-1 overflow-y-auto p-4">message</div>
+
+        <div className="absolute bottom-3 left-5 right-5 flex items-center gap-2 border-gray-300">
+            <textarea 
+                className="flex-1 resize-none p-2 bg-gray-400 rounded-lg focus:outline-none focus:ring focus:ring-blue-500"
+                rows={3}
+                placeholder="Type a message..."
+            />
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-lg opacity-50 cursor-pointer">Send</button>
+        </div>
+    </div>
+)
+}
